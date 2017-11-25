6ea9e762f402a89d517a05cec365a2d324332fe9
James Gary <bucket@earthlink.net>
2017-11-25 21:01:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 
 def won?(board)
   WIN_COMBINATIONS.each do |z|
-    if board [z[0]] != " "board [z[0]]==board [z[1]] && board [z[1]]==board [z[2]]
+    if board [z[0]] != " " && board [z[0]]==board [z[1]] && board [z[1]]==board [z[2]]
       return z
     end #of if statement
   end #of do loop
